e6eb2100bf855150aa1f88038c5fd7e95cf41aea
Michael Odermatt <43145047+MichaelOdermatt@users.noreply.github.com>
2023-08-16 16:54:11 -0600

    Added animated player sprite

--- Scripts/PlayerScripts/PlayerSprite.gd
+++ Scripts/PlayerScripts/PlayerSprite.gd
@@ -1,6 +1,10 @@
 extends Node2D
 
 @onready var spriteAnimationPlayer = $AnimationPlayer;
+@onready var playerSprite = $AnimatedSprite2D;
+
+func _ready():
+	playerSprite.play("default");
 
 ## Playes the On Hit animation for the player.
 func playOnHitAnimation():
