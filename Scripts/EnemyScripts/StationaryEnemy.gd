3fddddec80727591d51b992c759439c2c899a16b
Michael Odermatt <43145047+MichaelOdermatt@users.noreply.github.com>
2023-08-19 08:40:52 -0600

    Updated sprites
    - Updated player sprite colors
    
    - Added new enemy sprite

--- Scripts/EnemyScripts/StationaryEnemy.gd
+++ Scripts/EnemyScripts/StationaryEnemy.gd
@@ -1,7 +1,10 @@
 extends "res://Scripts/EnemyScripts/BasicEnemy.gd"
 
+@onready var trashSprite = $AnimatedSprite2D;
+
 func _ready():
 	getFlyingEnemySpeed();
+	trashSprite.play("default");
 
 ## Call the super function then give the enemy a random altitude.
 func resetEnemyPosition():
