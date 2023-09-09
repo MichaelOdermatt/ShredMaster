30007171e20ff35f82557e3a2d6f77110b21d697
Michael Odermatt <43145047+MichaelOdermatt@users.noreply.github.com>
2023-09-09 17:23:48 -0600

    Added cloud particles back
    - Added back cloud particles for when player dies. Just need to update the rest of the code for when the player dies

--- Scripts/PlayerScripts/PlayerParticleEffects.gd
+++ Scripts/PlayerScripts/PlayerParticleEffects.gd
@@ -3,6 +3,7 @@ extends Node2D
 @onready var globals = get_node("/root/Globals");
 @onready var impactParticles = $ImpactParticles;
 @onready var sparkParticles = $SparkParticles;
+@onready var cloudParticles = $CloudParticles;
 
 var impactParticleEffectOffsetY: float = 8;
 var sparkParticleEffectOffsetY: float = 7;
@@ -11,16 +12,17 @@ func _process(delta):
 	updateParticleVelocity();
 
 ## Emits the cloud particle effect.
+func emitCloudParticles(emitPosition: Vector2):
+	cloudParticles.restart();
+	cloudParticles.transform.origin = emitPosition;
+	cloudParticles.emitting = true;
+
+## Emits the impact particle effect.
 func emitImpactParticles(emitPosition: Vector2):
 	impactParticles.restart();
 	impactParticles.transform.origin = emitPosition;
 	impactParticles.transform.origin.y += impactParticleEffectOffsetY;
 	impactParticles.emitting = true;
-
-## Updates the velocity of all particle effects to match the global current base speed.
-func updateParticleVelocity():
-	impactParticles.material.set('initial_velocity_min', -globals.currentBaseSpeed);
-	impactParticles.material.set('initial_velocity_max', -globals.currentBaseSpeed);
 
 ## Starts emitting the spark particle effect at the given position.
 func startEmittingSparkParticles(emitPosition: Vector2):
@@ -31,3 +33,10 @@ func startEmittingSparkParticles(emitPosition: Vector2):
 ## Stops emitting the spark particle effect.
 func stopEmittingSparkParticles():
 	sparkParticles.emitting = false;
+
+## Updates the velocity of all particle effects to match the global current base speed.
+func updateParticleVelocity():
+	impactParticles.material.set('initial_velocity_min', -globals.currentBaseSpeed);
+	impactParticles.material.set('initial_velocity_max', -globals.currentBaseSpeed);
+	cloudParticles.material.set('initial_velocity_min', -globals.currentBaseSpeed);
+	cloudParticles.material.set('initial_velocity_max', -globals.currentBaseSpeed);
