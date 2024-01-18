b1875055c762648454315fc690a2bc743070a10b
Michael Odermatt <43145047+MichaelOdermatt@users.noreply.github.com>
2024-01-17 18:49:55 -0700

    Added pop can particles

--- Scripts/CoinScripts/Coin.gd
+++ Scripts/CoinScripts/Coin.gd
@@ -1,21 +1,22 @@
 extends Area2D
 
 @onready var sprite = $AnimatedSprite;
+@onready var collectCoinParticles = $CollectCoinParticles;
 
 func _ready():
 	body_entered.connect(_on_body_entered);
 	sprite.play("default");
 
-
 ## Enables and shows the coin
 func enable():
 	sprite.visible = true;
-	set_deferred('monitoring', true)
+	set_deferred('monitoring', true);
 
-## Disables and hides the coin.
+## Disables and hides the coin, then plays the particle effect.
 func disable():
 	sprite.visible = false;
-	set_deferred('monitoring', false)
+	set_deferred('monitoring', false);
+	collectCoinParticles.emitting = true;
 
 func _on_body_entered(body: Node2D):
 	disable();
